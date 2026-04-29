ab27bfb588f78e220f8d734b19ae2258aa1bca8e
lcmarquez2005 <lcarlosmarquezstrociak@gmail.com>
2026-04-28 18:29:08 -0600

    cambios hasta la fase 3 del codiog

---
+++ src/test/java/org/example/gymroutine/GymroutineApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.gymroutine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymroutineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
